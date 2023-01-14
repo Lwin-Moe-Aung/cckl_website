814227e3a9b9f8798518c05f90c7ca643b2b0925
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2023-01-14 15:59:12 +0630

    blog page pagination

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,15 +1,83 @@
-import React from 'react'
-
+import React,{ useEffect, useState } from 'react'
+import ReactPaginate from "react-paginate";
+import axios from '@/api/axios';
+import { BlogPostCard } from '@/widgets/cards';
+import "../utils/pagination.css";
 export const Blog = () => {
+
+  const [posts, setPosts] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+
+  let limit = 4;
+  const getPostUrl = "/admin/posts/all?page=";
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await axios.get(`${getPostUrl}0&size=${limit}`);
+      setPosts(res.data.data);
+      setPageCount(Math.ceil(res.data.total / limit));
+    };
+    getPosts();
+
+  }, [limit]);
+
+  const fetchPosts = async (currentPage) => {
+    const res = await axios.get(`${getPostUrl}${currentPage}&size=${limit}`);
+    return res.data.data;
+  }
+
+  const handlePageClick = async (data) => {
+    console.log(data.selected);
+
+    let currentPage = data.selected;
+
+    const dataFromServer = await fetchPosts(currentPage);
+
+    setPosts(dataFromServer);
+    // scroll to the top
+    //window.scrollTo(0, 0)
+  };
+
   return (
     <>
-        <section className="relative block h-[50vh]">
-            <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/background-1.jpg')] bg-cover bg-center" />
-            <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
-        </section>
-        <div>
-            blog page
+      <div className="xl:w-6/12 lg:w-9/12 w-full  xl:ml-6 lg:mr-6">
+
+        <div className="flex bg-white px-3 py-2 justify-between items-center rounded-sm mb-5">
+            <h5 className="text-base uppercase font-semibold font-roboto">BUSINESS</h5>
+            <a href="#"
+                className="text-white py-1 px-3 rounded-sm uppercase text-sm bg-blue-500 border border-blue-500 hover:text-blue-500 hover:bg-transparent transition">
+                see more
+            </a>
         </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+
+          {posts?.map((post, index) => (
+            <BlogPostCard key={index} post={post}/>
+          ))}
+
+        </div>
+
+        <ReactPaginate
+          previousLabel={"previous"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination justify-content-center"}
+          pageClassName={"page-item"}
+          pageLinkClassName={"page-link"}
+          previousClassName={"page-item"}
+          previousLinkClassName={"page-link"}
+          nextClassName={"page-item"}
+          nextLinkClassName={"page-link"}
+          breakClassName={"page-item"}
+          breakLinkClassName={"page-link"}
+          activeClassName={"active"}
+        />
+      </div>
     </>
   )
 }
--- src/widgets/cards/blog-post-card.jsx
+++ src/widgets/cards/blog-post-card.jsx
@@ -25,14 +25,14 @@ export function BlogPostCard({ post }) {
       </Link>
     
       <div className="mt-3">
-          <div className="mt-2 flex space-x-3 justify-end">
+          {/* <div className="mt-2 flex space-x-3 justify-end">
             <div className="flex text-gray-400 text-sm items-left">
               <span className="mr-2 text-xs">
                   <i className="far fa-clock"></i>
               </span>
               {fDate(post.createdAt)}
             </div>
-          </div>
+          </div> */}
           <Link
             to={blogDetailUrl}
             className="block rounded-md overflow-hidden"
@@ -43,7 +43,7 @@ export function BlogPostCard({ post }) {
             </h2>
           </Link>
         
-          <div className="flex space-x-3 justify-end">
+          {/* <div className="flex space-x-3 justify-end">
             <div className="flex text-gray-800 text-sm items-right">
               <span className="mr-2 text-xs">
                   <i className="far fa-eye"></i>
@@ -56,7 +56,27 @@ export function BlogPostCard({ post }) {
               </span>
               {fShortenNumber(post?.commentCount)}
             </div>
-          </div>
+          </div> */}
+          <div className="grid gap-1 grid-cols-3">
+            <div className="flex text-gray-400 text-sm items-center col-span-2">
+                <span className="mr-1 text-xs"><i className="far fa-clock"></i></span>
+                {fDate(post?.createdAt)}
+            </div>
+            <div className="flex space-x-3 justify-end">
+                <div className="flex text-gray-400 text-sm items-right">
+                    <span className="mr-2 text-xs">
+                        <i className="far fa-eye"></i>
+                    </span>
+                    {fShortenNumber(post?.view_count)}
+                </div>
+                <div className="flex text-gray-400 text-sm items-right">
+                    <span className="mr-2 text-xs">
+                        <i className="far fa-comment"></i>
+                    </span>
+                    {fShortenNumber(post?.commentCount)}
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   );
