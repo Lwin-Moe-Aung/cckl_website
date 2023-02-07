ee2dca55a76765044fd55f48aaf509d3af60c681
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2023-02-08 05:42:28 +0630

    change localhost to server ip

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'http://54.169.192.233:8080/api/';
 
 export default axios.create({
     baseURL: BASE_URL
